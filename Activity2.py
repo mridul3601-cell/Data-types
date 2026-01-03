3b79a278ac573c1c95250e227b52df8ccfa26d92
mridul3601-cell <mridul3601@gmail.com>
2026-01-03 18:10:17 -0500

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,12 @@
+Name = 'Mridul'
+age = 13
+is_student = True
+Weight = 36.5
+print ("type of name is" , type (Name))
+print ("type of age is" , type (age))
+print ("type of is_student is" , type (is_student))
+print ("type of weight is" , type (Weight))
+age = str(age)
+print ('type of age is', type(age))
+Weight= int (Weight)
+print ('type of weight is', type (Weight))
