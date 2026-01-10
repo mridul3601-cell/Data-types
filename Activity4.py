fe441bd5203c253fa828202b5c2addffa2d09cbd
mridul3601-cell <mridul3601@gmail.com>
2026-01-10 17:18:05 -0500

    Add files via upload

---
+++ Activity4.py
@@ -0,0 +1,9 @@
+birthdays = {}
+
+birthdays["Jamal"] = "10-01-2008"
+birthdays["Tyrone"] = "15-03-2008"
+birthdays["Kai"] = "22-07-2008"
+birthdays["Miles"] = "05-11-2008"
+birthdays["James"] = "30-12-2008"
+
+print(birthdays)
